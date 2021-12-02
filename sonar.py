3c7b33bcf4021ee4464f0494c2d87039f71ff1fe
Wesley Darlington <wesley.darlington@gmail.com>
2021-12-01 21:21:01 -0500

    Day 1 part 2

--- sonar.py
+++ sonar.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
-import sys
+### Day 1
+
+# Day 1 Part 1
 
 def count_increases(depths):
     increases = 0
@@ -9,8 +11,10 @@ def count_increases(depths):
             increases += 1
     return increases
 
+day1_test_input = [ 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 ]
+
 def test_count_increases():
-    got = count_increases([ 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 ])
+    got = count_increases(day1_test_input)
     want = 7
     if got != want:
         raise Exception('test_count_increases: got %d want %d' % (got, want))
@@ -21,4 +25,27 @@ def read_ints(filename):
     with open(filename) as f:
         return map(int, f)
 
-count_increases(read_ints('day01.input'))  # => 1482
+day1_input = read_ints('day01.input')
+day1a = count_increases(day1_input)  # => 1482
+print "Day 1 Part 1: %d" % day1a
+
+# Day 1 Part 2
+
+def count_sliding_increases(depths):
+    window = 3
+    increases = 0
+    prev = sum(depths[0:window])
+    for i in range(window, len(depths)):
+        next = prev + depths[i] - depths[i-window]
+        if next > prev:
+            increases += 1
+        prev = next
+    return increases
+
+got = count_sliding_increases(day1_test_input)
+want = 5
+if got != want:
+    raise Exception('test_count_increases: got %d want %d' % (got, want))
+
+day1b = count_sliding_increases(day1_input)
+print "Day 1 Part 2: %d" % day1b
