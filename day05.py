e4b0f9f3088bf2795ad945edccbfacf9084ba86a
Wesley Darlington <wesley.darlington@gmail.com>
2021-12-05 19:05:58 -0500

    day05

--- day05.py
+++ day05.py
@@ -104,3 +104,73 @@ with open("day05.input") as f:
     print('Day 5 part 1 :: num overlaps => %d' % noverlaps(day5_mesh))  # => 4873
 
 ### Part 2
+
+def sign(z):
+    if z < 0:
+        return -1
+    if z > 0:
+        return +1
+    return 0
+
+def diangley(lines):
+    """Like straight, but include diagonals at pi/4."""
+    mesh = collections.defaultdict(int)
+    for line in lines:
+        x1,y1,x2,y2 = line
+        if x1 == x2:
+            y = min(y1,y2)
+            while y <= max(y1,y2):
+                mesh[(x1,y)] += 1
+                y += 1
+        elif y1 == y2:
+            x = min(x1,x2)
+            while x <= max(x1,x2):
+                mesh[(x,y1)] += 1
+                x += 1
+        elif abs(y2 - y1) == abs(x2 - x1):
+            dx = sign(x2-x1)
+            dy = sign(y2-y1)
+            x, y = x1, y1
+            #print("(%d,%d) -> (%d,%d) @(%d,%d)" % (x1, y1, x2, y2, dx, dy))
+            done = False
+            while True:
+                #print("Setting (%d,%d)" % (x,y))
+                mesh[(x,y)] += 1
+                x += dx
+                y += dy
+                if done:
+                    break
+                if x == x2 and y == y2:
+                    done = True
+        else:
+            raise Exception('Skipping weird diagonal (%d,%d) -> (%d,%d)' % (x1,y1,x2,y2))
+    return mesh
+
+
+lines = parse(day5_test_input)
+mesh = diangley(lines)
+got = stringify_mesh(mesh)
+want = """\
+1.1....11.
+.111...2..
+..2.1.111.
+...1.2.2..
+.112313211
+...1.2....
+..1...1...
+.1.....1..
+1.......1.
+222111....
+"""
+if got != want:
+    raise Exception('stringify_mesh:\n\ngot:\n%s\nwant:\n%s\n' % (got, want))
+
+got = noverlaps(mesh)
+if got != 12:
+    raise Exception('test mesh has %d overlaps, want 12' % got)
+
+with open("day05.input") as f:
+    day5_input = f.read()
+    day5_lines = parse(day5_input)
+    day5_mesh = diangley(day5_lines)
+    print('Day 5 part 2 :: num overlaps => %d' % noverlaps(day5_mesh))  # => 19472
