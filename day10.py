43e62ce63d1fdecceab77098bb98b69d6847813a
Wesley Darlington <wesley.darlington@gmail.com>
2021-12-10 11:34:36 -0500

    day10 part 2

--- day10.py
+++ day10.py
@@ -14,9 +14,13 @@ test_input = """\
 """
 
 matches = {
+    '{': '}',
     '}': '{',
+    '[': ']',
     ']': '[',
+    '<': '>',
     '>': '<',
+    '(': ')',
     ')': '(',
 }
 
@@ -56,3 +60,52 @@ with open('day10.input') as f:
     print('Day 10 Part 1 => %d' % sc)  # => 240123
 
 ### Part 2
+
+cscores = {
+    ')': 1,
+    ']': 2,
+    '}': 3,
+    '>': 4,
+}
+
+def goodness(line):
+    stack = []
+    for c in line:
+        if c in ('(', '{', '[', '<'):
+            stack.append(c)
+        elif c in matches:
+            b = stack.pop()
+            if matches[c] != b:
+                return False
+    stack.reverse()
+    closers = [matches[c] for c in stack]
+    n = 0
+    for x in closers:
+        n = n*5 + cscores[x]
+    return n
+
+wants = {
+    '[({(<(())[]>[[{[]{<()<>>': 288957,
+    '[(()[<>])]({[<{<<[]>>(': 5566,
+    '(((({<>}<{<{<>}{[]{[]{}': 1480781,
+    '{<[[]]>}<{[{[{[]{()[[[]': 995444,
+    '<{([{{}}[<[[[<>{}]]]>[]]': 294,
+}
+for input, want in wants.items():
+    got = goodness(input)
+    if got != want:
+        raise Exception('goodness(%s): got %d, want %d' % (input, got, want))
+
+def best(input):
+    sc = list(filter(bool, map(goodness, input.splitlines())))
+    sc.sort()
+    return sc[len(sc)//2]
+
+got = best(test_input)
+want = 288957
+if got != want:
+    raise Exception('best(test_input): got %d, want %d' % (got, want))
+
+with open('day10.input') as f:
+    sc = best(f.read())
+    print('Day 10 Part 2 => %d' % sc)  # =>  3260812321
