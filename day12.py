4f7a8e625cdd8eda9d1fe634f310c9d92ec86233
Wesley Darlington <wesley.darlington@gmail.com>
2021-12-13 22:00:42 -0500

    Day 12 part 2
    Slow though. Shoudl find a non-quadratic way. Oh well...
    $ time ./day12.py
    Day 12 part 1 => 5254
    Day 12 part 2 => 149385
    ./day12.py  37.70s user 0.09s system 99% cpu 38.174 total

--- day12.py
+++ day12.py
@@ -36,7 +36,10 @@ def walk(graph, path=None, z='end'):
         yield from walk(graph, path2, z)
 
 def alphabetically(paths):
-  return '\n'.join(sorted([','.join(path) for path in paths])) + '\n'
+    return '\n'.join(sorted([','.join(path) for path in paths])) + '\n'
+
+def count(paths):
+    return sum(1 for x in paths)
 
 want = """\
 start,A,b,A,c,A,end
@@ -53,7 +56,7 @@ start,b,end
 got = alphabetically(walk(parse(test_input)))
 assert got == want, "first graph:\ngot:\n%s\nwant:\n%s\n" % (got, want)
 
-larger_example = """\
+slightly_larger_example = """\
 dc-end
 HN-start
 start-kj
@@ -65,7 +68,7 @@ kj-sa
 kj-HN
 kj-dc
 """
-got = alphabetically(walk(parse(larger_example)))
+got = alphabetically(walk(parse(slightly_larger_example)))
 want = """\
 start,HN,dc,HN,end
 start,HN,dc,HN,kj,HN,end
@@ -109,11 +112,80 @@ zg-he
 pj-fs
 start-RW
 """
-got = sum(1 for x in (walk(parse(even_larger_example))))
+got = count(walk(parse(even_larger_example)))
 want = 226
 assert got == want, "even_larger_example: got %d, want %d" % (got, want)
 
 with open('day12.input') as f:
     g = parse(f.read())
-    n = sum(1 for x in walk(g))
+    n = count(walk(g))
     print('Day 12 part 1 => %d' % n)
+
+### Part 2
+
+def no_little_dups(path):
+    smalls = list(filter(lambda x: x.islower(), path))
+    return len(smalls) == len(set(smalls))
+
+def candidates(graph, here, path):
+    for nxt in graph[here]:
+        if nxt.isupper():
+            yield nxt
+        elif nxt not in path:
+            yield nxt
+        elif no_little_dups(path) and nxt not in ('start', 'end'):
+            yield nxt
+
+want = """\
+start,A,b,A,b,A,c,A,end
+start,A,b,A,b,A,end
+start,A,b,A,b,end
+start,A,b,A,c,A,b,A,end
+start,A,b,A,c,A,b,end
+start,A,b,A,c,A,c,A,end
+start,A,b,A,c,A,end
+start,A,b,A,end
+start,A,b,d,b,A,c,A,end
+start,A,b,d,b,A,end
+start,A,b,d,b,end
+start,A,b,end
+start,A,c,A,b,A,b,A,end
+start,A,c,A,b,A,b,end
+start,A,c,A,b,A,c,A,end
+start,A,c,A,b,A,end
+start,A,c,A,b,d,b,A,end
+start,A,c,A,b,d,b,end
+start,A,c,A,b,end
+start,A,c,A,c,A,b,A,end
+start,A,c,A,c,A,b,end
+start,A,c,A,c,A,end
+start,A,c,A,end
+start,A,end
+start,b,A,b,A,c,A,end
+start,b,A,b,A,end
+start,b,A,b,end
+start,b,A,c,A,b,A,end
+start,b,A,c,A,b,end
+start,b,A,c,A,c,A,end
+start,b,A,c,A,end
+start,b,A,end
+start,b,d,b,A,c,A,end
+start,b,d,b,A,end
+start,b,d,b,end
+start,b,end
+"""
+got = alphabetically(walk(parse(test_input)))
+assert got == want, "simple input (36), one little dup allowed: (-want +got):\n%s" % ('\n'.join(difflib.unified_diff(want.splitlines(), got.splitlines())))
+
+got = count(walk(parse(slightly_larger_example)))
+want = 103
+assert got == want, "slightly larger example, one wee dup ok: got %d, want %d" % (got, want)
+
+got = count(walk(parse(even_larger_example)))
+want = 3509
+assert got == want, "even larger example, one wee dup ok: got %d, want %d" % (got, want)
+
+with open('day12.input') as f:
+    g = parse(f.read())
+    n = count(walk(g))
+    print('Day 12 part 2 => %d' % n)  # => 149385  (Slow: tens of seconds!!
