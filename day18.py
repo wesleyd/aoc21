e71e8ddc3a5c5df6718ee6475ecb606f34c1fbbe
Wesley Darlington <wesley.darlington@gmail.com>
2021-12-18 16:44:37 -0500

    day18 part 2 ... still disgusting, but at least I can forget about it now

--- day18.py
+++ day18.py
@@ -172,3 +172,30 @@ assert magnitude(snailaddlines("""\
 
 with open('inputs/day18.input') as f:
     print("Day 18 part 1 => %s" % magnitude(snailaddlines(f.read())))
+
+# Part 2
+
+def pairwise_magnitudes(ss):
+    lines = ss.splitlines()
+    for i in range(len(lines)):
+        for j in range(len(lines)):
+            if i == j:
+                continue
+            yield magnitude(snailadd(lines[i], lines[j]))
+
+assert max(pairwise_magnitudes("""\
+[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+[[[5,[2,8]],4],[5,[[9,9],0]]]
+[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+[[[[5,4],[7,7]],8],[[8,3],8]]
+[[9,3],[[9,9],[6,[4,9]]]]
+[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]
+""")) == 3993
+
+
+with open('inputs/day18.input') as f:
+    print("Day 18 part 2 => %s" % max(pairwise_magnitudes(f.read())))
